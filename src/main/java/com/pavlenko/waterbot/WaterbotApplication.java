046d205bdc40c63ba70d585c1059e2d6dd84f74e
Rostislav <rospavlenko@gmail.com>
2017-12-29 19:40:31 +0200

    project basic set up

---
+++ src/main/java/com/pavlenko/waterbot/WaterbotApplication.java
@@ -0,0 +1,12 @@
+package com.pavlenko.waterbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaterbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaterbotApplication.class, args);
+	}
+}
